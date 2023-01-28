68a2411a1dfea81564cd3d3845751bb5bd919714
nesova <119742990+nesova@users.noreply.github.com>
2023-01-28 14:47:04 +0300

    demo ver 1

--- main.py
+++ main.py
@@ -1,63 +1,96 @@
 import pygame
+import sys
 
-MAP = [
-    '#######@###',
-    '#         #',
-    '# ### #####',
-    '# # #     #',
-    '# # # ### #',
-    '#K# # #K# #',
-    '### # # # #',
-    '#   # #   #',
-    '# # # # # #',
-    '# #   # #K#',
-    '#X#########',
-]
+FPS = 50
+WIDTH = HEIGHT = 510
 
-BLOCK_SIDE = 50
-CELL_SIDE = 50
-WIDTH = len(MAP[0])
-HEIGHT = len(MAP)
-SCREEN_WIDTH = WIDTH * BLOCK_SIDE
-SCREEN_HEIGHT = HEIGHT * BLOCK_SIDE
+tile_images = {
+    'wall': pygame.image.load(f'images/wall.png'),
+    'empty': pygame.image.load(f'images/ground.png')
+}
+
+tile_width = tile_height = 50
+player_image = pygame.image.load(f'images/player.png')
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
 
 
-class Wall:
-    def __init__(self):
-        self.pos = []
-        self.texture = pygame.image.load(f'images/wall.png')
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    return new_player, x, y
 
-    def get_coord(self, x, y):
-        self.pos.append((x, y))
 
-    def draw(self):
-        for pos in self.pos:
-            screen.blit(self.texture, pos)
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
 
 
-class Maze:
-    def __init__(self):
-        self.wall = Wall()
-        for i in range(HEIGHT):
-            for j in range(WIDTH):
-                if MAP[i][j] == '#':
-                    self.wall.get_coord(j * BLOCK_SIDE, i * BLOCK_SIDE)
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
 
-    def draw(self):
-        self.wall.draw()
 
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    return level_map
+
+
+def start_screen():
+    intro_text = ["MAZE", "",
+                  "Нажмите на экран, чтобы начать"]
+
+    background = pygame.transform.scale(pygame.image.load(f'images/background.png'), (WIDTH, HEIGHT))
+    screen.blit(background, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+player, level_x, level_y = generate_level(load_level('map_1.txt'))
 
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    font = pygame.font.SysFont('arial', 60)
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        maze = Maze()
-        maze.draw()
-        pygame.display.flip()
-    pygame.quit()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    start_screen()
