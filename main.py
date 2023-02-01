9a54a76b7b7b21d911df7437db00e343689b4b80
nesova <119742990+nesova@users.noreply.github.com>
2023-02-01 21:07:00 +0300

    key take, player animation, door image

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 key_group = pygame.sprite.Group()
+exit_group = pygame.sprite.Group()
 
 
 def generate_level(level):
@@ -28,9 +29,9 @@ def generate_level(level):
             elif level[y][x] == '#':
                 Tile('wall', x, y)
             elif level[y][x] == 'K':
-                pass
+                Key(x, y)
             elif level[y][x] == "X":
-                pass
+                Exit(x, y)
             elif level[y][x] == '@':
                 Tile('empty', x, y)
                 new_player = Player(x, y)
@@ -45,21 +46,45 @@ class Tile(pygame.sprite.Sprite):
             tile_width * pos_x, tile_height * pos_y)
 
 
+class Exit(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(exit_group, all_sprites)
+        self.frames = []
+        self.door_im = pygame.transform.scale(pygame.image.load(f'images/door.png'), (tile_width * 4, tile_height))
+        self.cut_sheet(self.door_im, 4, 1)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+
 class Key(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(key_group, all_sprites)
         self.image = key_image
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
-        self.taken = False
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
         self.frames = []
-        self.down = pygame.transform.scale(pygame.image.load(f'images/player/down.png'), (400, 100))
-        self.cut_sheet(self.down, 4, 1)
+        self.move_im = pygame.transform.scale(pygame.image.load(f'images/player.png'), (tile_width * 4, tile_height))
+        self.cut_sheet(self.move_im, 4, 1)
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
         self.count = 0
@@ -86,17 +111,21 @@ class Player(pygame.sprite.Sprite):
         speed = 2
         if keys[pygame.K_LSHIFT]:
             speed = 4
-        if keys[pygame.K_LEFT]:
-            self.rect = self.rect.move(-speed, 0)
-        if keys[pygame.K_RIGHT]:
-            self.rect = self.rect.move(speed, 0)
-        if keys[pygame.K_UP]:
-            self.rect = self.rect.move(0, -speed)
-        if keys[pygame.K_DOWN]:
-            self.rect = self.rect.move(0, speed)
+        if True in keys:
+            if keys[pygame.K_LEFT]:
+                self.rect = self.rect.move(-speed, 0)
+            if keys[pygame.K_RIGHT]:
+                self.rect = self.rect.move(speed, 0)
+            if keys[pygame.K_UP]:
+                self.rect = self.rect.move(0, -speed)
+            if keys[pygame.K_DOWN]:
+                self.rect = self.rect.move(0, speed)
             if self.counter():
                 self.cur_frame = (self.cur_frame + 1) % len(self.frames)
                 self.image = self.frames[self.cur_frame]
+        else:
+            self.cur_frame = 0
+            self.image = self.frames[self.cur_frame]
 
 
 def terminate():
@@ -144,11 +173,15 @@ def level_1():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
+        ratio = pygame.sprite.collide_rect_ratio(0.5)
+        pygame.sprite.groupcollide(player_group, key_group, False, True, ratio)
         keys = pygame.key.get_pressed()
         player.update(keys)
 
         screen.fill((0, 0, 0))
+        exit_group.draw(screen)
         tiles_group.draw(screen)
+        key_group.draw(screen)
         player_group.draw(screen)
 
         pygame.display.flip()
