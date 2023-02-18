44f0810b17cc82733238deaf0702fb1ab0f50ae8
nesova <119742990+nesova@users.noreply.github.com>
2023-02-18 17:00:13 +0300

    3 levels, win message, die message, open door

--- main.py
+++ main.py
@@ -3,18 +3,12 @@ import sys
 
 FPS = 50
 size = (width, height) = (500, 500)
+tile_size = (tile_width, tile_height) = (100, 100)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
-tile_size = (tile_width, tile_height) = (100, 100)
-key_image = pygame.transform.scale(pygame.image.load(f'images/key.png'), tile_size)
-tile_images = {
-    'wall': pygame.transform.scale(pygame.image.load(f'images/wall.png'), tile_size),
-    'empty': pygame.transform.scale(pygame.image.load(f'images/ground.png'), tile_size)
-}
-
 all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
+poison_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 key_group = pygame.sprite.Group()
 exit_group = pygame.sprite.Group()
@@ -24,24 +18,21 @@ def generate_level(level):
     new_player, x, y = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#':
-                Tile('wall', x, y)
+            if level[y][x] == '#':
+                Poison(x, y)
             elif level[y][x] == 'K':
                 Key(x, y)
             elif level[y][x] == "X":
                 Exit(x, y)
             elif level[y][x] == '@':
-                Tile('empty', x, y)
                 new_player = Player(x, y)
     return new_player, x, y
 
 
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
+class Poison(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(poison_group, all_sprites)
+        self.image = pygame.transform.scale(pygame.image.load(f'images/poison.png'), tile_size)
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
 
@@ -56,6 +47,7 @@ class Exit(pygame.sprite.Sprite):
         self.image = self.frames[self.cur_frame]
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
+        self.open = False
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
@@ -74,7 +66,7 @@ class Exit(pygame.sprite.Sprite):
 class Key(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(key_group, all_sprites)
-        self.image = key_image
+        self.image = pygame.transform.scale(pygame.image.load(f'images/key.png'), tile_size)
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
 
@@ -90,12 +82,6 @@ class Player(pygame.sprite.Sprite):
         self.count = 0
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
-        self.y = pos_y
-        self.x = pos_x
-        self.shift_left = 0
-        self.shift_right = 0
-        self.shift_up = 0
-        self.shift_down = 0
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
@@ -118,29 +104,13 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_LSHIFT]:
             speed = 4
         if True in keys:
-            if keys[pygame.K_LEFT] and level_map[self.y][self.x - 1] != '#':
-                self.shift_left += speed
-                if self.shift_left == 100:
-                    self.x -= 1
-                    self.shift_left = 0
+            if keys[pygame.K_LEFT]:
                 self.rect = self.rect.move(-speed, 0)
-            if keys[pygame.K_RIGHT] and level_map[self.y][self.x + 1] != '#':
-                self.shift_right += speed
-                if self.shift_right == 100:
-                    self.x += 1
-                    self.shift_right = 0
+            if keys[pygame.K_RIGHT]:
                 self.rect = self.rect.move(speed, 0)
-            if keys[pygame.K_UP] and level_map[self.y - 1][self.x] != '#':
-                self.shift_up += speed
-                if self.shift_up == 100:
-                    self.y -= 1
-                    self.shift_up = 0
+            if keys[pygame.K_UP]:
                 self.rect = self.rect.move(0, -speed)
-            if keys[pygame.K_DOWN] and level_map[self.y + 1][self.x] != '#':
-                self.shift_down += speed
-                if self.shift_down == 100:
-                    self.y += 1
-                    self.shift_down = 0
+            if keys[pygame.K_DOWN]:
                 self.rect = self.rect.move(0, speed)
             if self.counter():
                 self.cur_frame = (self.cur_frame + 1) % len(self.frames)
@@ -148,46 +118,6 @@ class Player(pygame.sprite.Sprite):
         else:
             self.cur_frame = 0
             self.image = self.frames[self.cur_frame]
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
-def load_level(filename):
-    filename = "data/" + filename
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-    return level_map
-
-
-def start_screen():
-    intro_text = ["MAZE", "",
-                  "Нажмите на экран, чтобы начать"]
-
-    background = pygame.transform.scale(pygame.image.load(f'images/background.png'), size)
-    screen.blit(background, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return
-        pygame.display.flip()
-        clock.tick(FPS)
 
 
 class Camera:
@@ -204,22 +134,82 @@ class Camera:
         self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
 
 
-def level_1():
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    return level_map
+
+
+def text(message, size, move_x, move_y):
+    font = pygame.font.Font("data/font.ttf", size)
+    text = font.render(message, True, pygame.Color('white'))
+    textRect = text.get_rect()
+    textRect.center = (move_x, move_y)
+    screen.blit(text, textRect)
+
+
+def start_screen():
+    background = pygame.transform.scale(pygame.image.load(f'images/background.png'), size)
+    screen.blit(background, (0, 0))
+    text(" MAZE", 80, width // 5, height // 7 * 2)
+    text("Нажмите, чтобы начать", 30, width // 5 * 2, height // 7 * 3)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def final_window(message):
+    screen.fill((0, 0, 0))
+    text(message, 40, width // 2, height // 2)
+    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+
+
+def level():
     camera = Camera()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-        ratio = pygame.sprite.collide_rect_ratio(0.5)
+        ratio = pygame.sprite.collide_rect_ratio(0.7)
         pygame.sprite.groupcollide(player_group, key_group, False, True, ratio)
-        keys = pygame.key.get_pressed()
-        player.update(keys)
+        if pygame.sprite.groupcollide(player_group, poison_group, False, False, ratio) == {}:
+            keys = pygame.key.get_pressed()
+            player.update(keys)
+        else:
+            final_window("Вас настигла смерть...")
+
+        if pygame.sprite.groupcollide(player_group, exit_group, False, False, ratio) != {}:
+            if str(key_group) == "<Group(0 sprites)>":
+                for _ in range(3):
+                    exit_group.update()
+                    exit_group.draw(screen)
+                    pygame.display.flip()
+                    pygame.time.wait(300)
+                return
+
         camera.update(player)
         for sprite in all_sprites:
             camera.apply(sprite)
         screen.fill((0, 0, 0))
         exit_group.draw(screen)
-        tiles_group.draw(screen)
+        poison_group.draw(screen)
         key_group.draw(screen)
         player_group.draw(screen)
 
@@ -230,6 +220,12 @@ def level_1():
 if __name__ == '__main__':
     pygame.init()
     start_screen()
-    level_map = load_level("map_2.txt")
-    player, level_x, level_y = generate_level(load_level("map_2.txt"))
-    level_1()
+    for i in range(1, 4):
+        poison_group.empty()
+        exit_group.empty()
+        key_group.empty()
+        player_group.empty()
+        level_map = load_level(f"map_{i}.txt")
+        player, level_x, level_y = generate_level(load_level(f"map_{i}.txt"))
+        level()
+    final_window("Вы успешно выбрались!")
